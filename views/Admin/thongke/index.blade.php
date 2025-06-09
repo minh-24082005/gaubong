434b682cd3a20a14559e2b6f6eddbe07db41e441
minh-24082005 <minhnvph49879@gmail.com>
2025-06-09 17:04:39 +0700

    thống kê

--- views/Admin/thongke/index.blade.php
+++ views/Admin/thongke/index.blade.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<h2>Thống kê tồn kho sản phẩm</h2>
+<table border="1" cellpadding="5">
+    <tr>
+        <th>id</th>
+        <th>Tên sản phẩm</th>
+        <th>Kích cỡ</th>
+        <th>Giá</th>
+        <th>Số lượng tồn kho</th>
+    </tr>
+@foreach ($data as $item)
+    <tr>
+        <td>{{ $item['ma_san_pham'] }}</td>
+        <td>{{ $item['ten_san_pham'] }}</td>
+        <td>{{ $item['kich_co'] }}cm</td>
+        <td>{{ number_format($item['giaban'] ?? 0) }}
+</td>
+        <td>{{ $item['so_luong_ban_thang'] }}</td>
+        <td>{{ number_format($item['doanh_thu']) }}</td>
+        <td>{{ $item['ton_kho'] }}</td>
+    </tr>
+@endforeach
+
+
+</table>
